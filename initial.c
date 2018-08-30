0dd5760fd64e62525fb86f00c41dbfbe3776a602
komal1231 <42837473+komal1231@users.noreply.github.com>
2018-08-30 18:34:21 +0530

    Create initial.c

---
+++ initial.c
@@ -0,0 +1,16 @@
+what is a programme?
+why is c necessary?
+what is int actully used for?
+what is a compiler?
+where do we type a programme?
+why is semicolon used after every statement?
+how can i run c programme?
+what rules are to be followed to write a programme?
+how to end a programme?
+why should two open brackets be used after main?
+when we use printf,why should we put everything under double quotations?
+what is stdio.h? 
+why should we start with #include<stdio.h> ?
+why should we only use return 0 ?
+
+
